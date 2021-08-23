0a3d6b2b3fe82d7a3307b2cbe57a5f9e56644f93
Luka <vujnovacluka89@gmail.com>
2021-08-23 13:03:48 +0200

    Initial Commit

---
+++ Skola Nogometa/Skola_NogometaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skola_NogometaApp.swift
+//  Skola Nogometa
+//
+//  Created by Luka Vujnovac on 23.08.2021..
+//
+
+import SwiftUI
+
+@main
+struct Skola_NogometaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
